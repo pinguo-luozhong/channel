3caa38bbbad1aa0a3cb9a1d16169f57949737d35
luozhong <luozhong@luozhong.local>
2016-01-25 13:54:31 +0800

    update

--- routes/getChannelList.js
+++ routes/getChannelList.js
@@ -29,10 +29,12 @@ module.exports = function (app) {
 
             console.log(result);
             if (params.query && params.query.callback) {
+                console.log("//jsonp");
                 //console.log(params.query.callback);
                 var str = params.query.callback + '(' + JSON.stringify(result) + ')';//jsonp
                 res.end(str);
             } else {
+                console.log("//普通的json");
                 res.end(JSON.stringify(result));//普通的json
             }
         });
