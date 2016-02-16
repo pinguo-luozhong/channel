a276a3006008cfa0902d6f41646943733cd181c6
luozhong <luozhong@luozhong.local>
2016-02-16 16:51:17 +0800

    change path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 
 // 设定views变量，意为视图存放的目录
-app.set('views', path.join(__dirname, 'public/dist'));
+app.set('views', path.join(__dirname, 'public'));
 
 // 设定view engine变量，意为网页模板引擎
 //app.set('view engine', 'ejs');
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // 设定静态文件目录，比如本地文件
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 require('./routes')(app);
 
 app.get('/', function (req, res) {
